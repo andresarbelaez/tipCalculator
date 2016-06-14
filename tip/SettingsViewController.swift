2f4735ad421db9a6ad063ee317b35750279ea7cc
Andres Arbelaez <dres@fb.com>
2016-06-14 15:42:33 -0700

    Initial commit of project

---
+++ tip/SettingsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SettingsViewController.swift
+//  tip
+//
+//  Created by Andrés Arbeláez on 6/14/16.
+//  Copyright © 2016 Andrés Arbeláez. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    
+        
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewWillAppear(animated)
+       
+    
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -9,7 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        defaults.setDouble(0.2, forKey: "default_tip_percentage")
+        
+        defaults.synchronize()
+        
+        // This is a good place to retrieve the default tip percentage from NSUserDefaults
+        // and use it to update the tip amount
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        let tipValue = defaults.doubleForKey("default_tip_percentage")
+        
+        self.tipControl.alpha = 0
+        self.billField.alpha = 1
+        UIView.animateWithDuration(2.4, animations: {
+            self.tipControl.alpha = 1
+            self.billField.alpha = 0
+        })
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +59,17 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func calculateTip(sender: AnyObject) {
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        tipLabel.text = String(format:  "$%.2f", tip)
+        totalLabel.text = String(format:  "$%.2f", total)
+    }
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
